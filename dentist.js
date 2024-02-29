7dc3dbcb879b4e7226c761bce4ac9460fd5b288e
Ladan Torkzaban <99889241+ladantork@users.noreply.github.com>
2024-02-29 11:49:25 +0000

    Create dentist.js

---
+++ dentist.js
@@ -0,0 +1,11 @@
+function findFirstDentist(people) {
+// Your code goes here...
+
+for(let i =0; i<people.length; i++){
+    if(people[i].isDentist){
+return people[i]
+    }
+
+    }
+    return null;
+    }
